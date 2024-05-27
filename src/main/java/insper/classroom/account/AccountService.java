7989ba4110b1f79a898567901d8db7004824a50a
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-05-26 23:02:07 -0300

    att circuit break account

--- src/main/java/insper/classroom/account/AccountService.java
+++ src/main/java/insper/classroom/account/AccountService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.NonNull;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @Service
 public class AccountService {
@@ -16,19 +17,34 @@ public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
 
+    @CircuitBreaker(name = "accountService", fallbackMethod = "fallbackAccountCreate")
     public Account create(Account in) {
         in.hash(calculateHash(in.password()));
         in.password(null);
         return accountRepository.save(new AccountModel(in)).to();
     }
 
+    public Account fallbackAccountCreate(Account in, Throwable t) {
+        throw new RuntimeException("Failed to create account", t);
+    }
+
+    @CircuitBreaker(name = "accountService", fallbackMethod = "fallbackAccountRead")
     public Account read(@NonNull String id) {
         return accountRepository.findById(id).map(AccountModel::to).orElse(null);
     }
 
+    public Account fallbackAccountRead(String id, Throwable t) {
+        throw new RuntimeException("Failed to read account", t);
+    }
+
+    @CircuitBreaker(name = "accountService", fallbackMethod = "fallbackAccountReadByEmail")
     public Account login(String email, String password) {
         String hash = calculateHash(password);
         return accountRepository.findByEmailAndHash(email, hash).map(AccountModel::to).orElse(null);
+    }
+
+    public Account fallbackAccountReadByEmail(String email, String password, Throwable t) {
+        throw new RuntimeException("Failed to read account by email", t);
     }
 
     private String calculateHash(String text) {
